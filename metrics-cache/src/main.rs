f86dedaa223855b9d6d18d0050fa224d0dc42a86
Rick Elrod <rick.elrod@checkmk.com>
2026-05-01 19:08:07 +0200

    Hello axum world

--- metrics-cache/src/main.rs
+++ metrics-cache/src/main.rs
@@ -1,6 +1,11 @@
-use cmk_kube_types;
+use axum::{Router, routing::get};
 
-fn main() {
-    let foo = cmk_kube_types::add(2, 3);
-    println!("Hello, world! {}", foo);
+// use cmk_kube_types;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "foo" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
