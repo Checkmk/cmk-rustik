c668b3d448589e84bca06307ce21f8f4ab591f2d
Rick Elrod <rick.elrod@checkmk.com>
2026-06-19 00:08:52 +0200

    m-c: Resources for Namespace

--- metrics-cache/src/piggyback/namespace.rs
+++ metrics-cache/src/piggyback/namespace.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
 use k8s_openapi::api::core::v1;
+use std::ops::Add;
 
 use crate::piggyback::{Meta, PiggybackHost};
 use crate::section::{
     namespace::KubeNamespaceInfoV1,
+    resource::{KubeCpuResourcesV1, KubeMemoryResourcesV1, ResourceAccumulator, ResourceAxis},
     writeable::{SectionError, WriteableSection},
 };
 use crate::snapshot::Snapshot;
@@ -39,11 +41,37 @@ impl Namespace<'_> {
             kubernetes_cluster_hostname: "TODO_CLUSTERNAME", // TODO
         }
     }
+
+    fn cpu_resources(&self) -> KubeCpuResourcesV1 {
+        let ra = self
+            .snapshot
+            .owner_graph
+            .pods_by_namespace(self.meta.name)
+            .iter()
+            .map(|p| ResourceAccumulator::from_pod(p, ResourceAxis::Cpu))
+            .fold(ResourceAccumulator::default(), Add::add);
+        KubeCpuResourcesV1(ra)
+    }
+
+    fn memory_resources(&self) -> KubeMemoryResourcesV1 {
+        let ra = self
+            .snapshot
+            .owner_graph
+            .pods_by_namespace(self.meta.name)
+            .iter()
+            .map(|p| ResourceAccumulator::from_pod(p, ResourceAxis::Memory))
+            .fold(ResourceAccumulator::default(), Add::add);
+        KubeMemoryResourcesV1(ra)
+    }
 }
 
 impl PiggybackHost for Namespace<'_> {
     fn emit(&self) -> Vec<Result<WriteableSection, SectionError>> {
         let me = self.meta.piggyback_hostname("TODO_CLUSTERNAME");
-        vec![WriteableSection::of(me, &self.info())]
+        vec![
+            WriteableSection::of(me.clone(), &self.info()),
+            WriteableSection::of(me.clone(), &self.cpu_resources()),
+            WriteableSection::of(me, &self.memory_resources()),
+        ]
     }
 }
