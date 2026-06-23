13302be16d4883d536ba23aaded4771d9a3b6fd6
Rick Elrod <rick.elrod@checkmk.com>
2026-06-23 17:07:35 +0200

    m-c: parse volumes in kubelet scrape
    - Also update fixture to have a pod with a PVC
    - And move tests to be next to the data types

--- metrics-cache/src/handlers/ingest.rs
+++ metrics-cache/src/handlers/ingest.rs
@@ -18,37 +18,3 @@ pub async fn kubelet_stats_summary(
         .await;
     Json("ok".to_string())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use anyhow::Result;
-    use std::assert_matches;
-
-    const STATS_SUMMARY: &str =
-        include_str!("../../tests/fixtures/ingress/kubelet_stats_summary.json");
-
-    #[test]
-    fn test_parses_kubelet_stats_summary() -> Result<()> {
-        let parsed: StatsSummary = serde_json::from_str(STATS_SUMMARY)?;
-        assert_eq!(parsed.pods.len(), 11);
-        assert_eq!(parsed.node.node_name, "rustik-control-plane");
-        assert_eq!(parsed.pods[3].pod_ref.name, "coredns-589f44dc88-48xcj");
-        assert_eq!(parsed.pods[3].containers[0].name, "coredns");
-        assert_matches!(
-            parsed.pods[3].containers[0]
-                .cpu
-                .as_ref()
-                .and_then(|c| c.usage_nano_cores),
-            Some(3644771)
-        );
-        assert_matches!(
-            parsed.pods[3].containers[0]
-                .memory
-                .as_ref()
-                .and_then(|m| m.working_set_bytes),
-            Some(15020032)
-        );
-        Ok(())
-    }
-}
--- metrics-cache/src/ingest/kubelet_stats.rs
+++ metrics-cache/src/ingest/kubelet_stats.rs
@@ -18,6 +18,7 @@ pub struct Pod {
     #[serde(rename = "podRef")]
     pub pod_ref: PodReference,
     pub containers: Vec<Container>,
+    pub volume: Option<Vec<Volume>>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -43,4 +44,73 @@ pub struct CPUStats {
 pub struct MemoryStats {
     #[serde(rename = "workingSetBytes")]
     pub working_set_bytes: Option<u64>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Volume {
+    #[serde(rename = "availableBytes")]
+    pub available_bytes: u64,
+    #[serde(rename = "capacityBytes")]
+    pub capacity_bytes: u64,
+    #[serde(rename = "pvcRef")]
+    pub pvc_ref: Option<PVCRef>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct PVCRef {
+    pub name: String,
+    pub namespace: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::StatsSummary;
+    use anyhow::Result;
+    use std::assert_matches;
+
+    const STATS_SUMMARY: &str =
+        include_str!("../../tests/fixtures/ingress/kubelet_stats_summary.json");
+
+    #[test]
+    fn test_parses_kubelet_stats_summary() -> Result<()> {
+        let parsed: StatsSummary = serde_json::from_str(STATS_SUMMARY)?;
+        assert_eq!(parsed.pods.len(), 14);
+        assert_eq!(parsed.node.node_name, "rustik-control-plane");
+        assert_eq!(
+            parsed.pods[3].pod_ref.name,
+            "metrics-cache-5d466b6446-jdjt8"
+        );
+        assert_eq!(parsed.pods[3].containers[0].name, "metrics-cache");
+        assert_matches!(
+            parsed.pods[3].containers[0]
+                .cpu
+                .as_ref()
+                .and_then(|c| c.usage_nano_cores),
+            Some(2232140)
+        );
+        assert_matches!(
+            parsed.pods[3].containers[0]
+                .memory
+                .as_ref()
+                .and_then(|m| m.working_set_bytes),
+            Some(10706944)
+        );
+        assert_matches!(
+            parsed.pods[4].volume.as_ref().map(|m| m[0].available_bytes),
+            Some(349491232768)
+        );
+        assert_matches!(
+            parsed.pods[4].volume.as_ref().map(|m| m[0].capacity_bytes),
+            Some(1003736440832)
+        );
+        assert_matches!(
+            parsed.pods[4]
+                .volume
+                .as_ref()
+                .and_then(|m| m[0].pvc_ref.as_ref())
+                .map(|o| o.name.as_str()),
+            Some("pvc-csi-test")
+        );
+        Ok(())
+    }
 }
