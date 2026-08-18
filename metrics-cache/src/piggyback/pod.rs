dfdb1026cbf190deb40ff20dade86d7b0992625f
racicLuka <luka.racic@checkmk.com>
2026-08-18 13:56:44 +0200

    feat: migrate section kube_pod_init_containers_v1
    CMK-36257

--- metrics-cache/src/piggyback/pod.rs
+++ metrics-cache/src/piggyback/pod.rs
@@ -5,7 +5,8 @@ use std::sync::Arc;
 use crate::host_settings::HostSettings;
 use crate::piggyback::{AggregationHost, Meta, PiggybackHost};
 use crate::section::pod::{
-    KubePodConditionsV1, KubePodContainersV1, KubePodInfoV1, KubePodLifecycleV1, KubeStartTimeV1,
+    KubePodConditionsV1, KubePodContainersV1, KubePodInfoV1, KubePodInitContainersV1,
+    KubePodLifecycleV1, KubeStartTimeV1,
 };
 use crate::section::pvc::{KubePvcPvsV1, KubePvcV1, KubePvcVolumesV1};
 use crate::section::writeable::{SectionError, WriteableSection};
@@ -112,6 +113,9 @@ impl PiggybackHost for Pod<'_> {
         }
         if let Some(kube_pod_containers_v1) = KubePodContainersV1::from_pod(self.api) {
             out.push(WriteableSection::of(&me, &kube_pod_containers_v1));
+        }
+        if let Some(kube_pod_init_containers_v1) = KubePodInitContainersV1::from_pod(self.api) {
+            out.push(WriteableSection::of(&me, &kube_pod_init_containers_v1));
         }
 
         out
